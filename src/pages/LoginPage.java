7de57e399bb5643fd575df98aa38a35f71b148a4
Sathish0107 <sathishbaskaranr@gmail.com>
2023-01-26 20:28:49 +0530

    corrections done

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,7 +4,9 @@ public class LoginPage {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("this is login page");
+		System.out.println("this is login page and validation done");
+		
+		System.out.println("amazon apps is developed");
 	}
 
 }
